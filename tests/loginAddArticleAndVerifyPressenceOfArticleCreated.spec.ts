1c61092ca1c59c9f17422824b98510c89d4bdd3b
Gilberto Rincon <grincon1994@gmail.com>
2025-11-10 01:34:18 -0500

    updated

--- tests/loginAddArticleAndVerifyPressenceOfArticleCreated.spec.ts
+++ tests/loginAddArticleAndVerifyPressenceOfArticleCreated.spec.ts
@@ -3,7 +3,6 @@ import { EditorPage } from '../src/pages/EditorPage';
 import { ArticlePage } from '../src/pages/ArticlePage';
 import { faker } from '@faker-js/faker';
 
-test.describe('Create Article, verify presence, navigate to favorite posts and delete article', () => {
 
     test('create article, check presence of article title, navigate to favorite posts and delete article created', async ({ page, request }) => {
         const randomTitle = faker.lorem.words({ min: 2, max: 4 })
@@ -13,26 +12,27 @@ test.describe('Create Article, verify presence, navigate to favorite posts and d
 
         const editor = new EditorPage(page)
         await editor.createNewArticle(randomTitle, randomArticleSubject, randomArticleDescription, randomArticleTags);
+        // await page.waitForTimeout(1000);
+
+        const article = new ArticlePage(page)
+        await article.verifyArticleIsDisplayed(randomTitle);
+        await article.toggleFavoriteArticle();
+        await page.locator('.navbar-light .nav-link', { hasText: 'standard_user' }).click();
+        await page.locator('.articles-toggle .nav-link', { hasText: 'Favorited Posts' }).click();
+        await expect(page.getByRole('heading', { name: randomTitle })).toHaveText(randomTitle);
+
         const articlePageResponse = await page.waitForResponse('https://conduit-api.bondaracademy.com/api/articles/')
         const articleResponseBody = await articlePageResponse.json();
         const articleSlugId = articleResponseBody.article.slug;
 
-        // await page.waitForTimeout(1000);
-
-        // const article = new ArticlePage(page)
-        // await article.verifyArticleIsDisplayed(randomTitle);
-        // await article.toggleFavoriteArticle();
-        // await page.locator('.navbar-light .nav-link', { hasText: 'standard_user' }).click();
-        // await page.locator('.articles-toggle .nav-link', { hasText: 'Favorited Posts' }).click();
-        // await expect(page.getByRole('heading', { name: randomTitle })).toHaveText(randomTitle);
-
 
         
-        const deleteArticleResponse = await request.delete('https://conduit-api.bondaracademy.com/api/articles/cerno-sponte-theologus-38023')
-        await expect(deleteArticleResponse.status()).toBe(204);
+        const deleteArticleResponse = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${articleSlugId}`, {
+            headers:{
+                'Authorization': `Token ${process.env.ACCESS_TOKEN}`
+            }
+        })
+        // await expect(deleteArticleResponse.status()).toBe(204);
     })
-
-})
-
 
 
